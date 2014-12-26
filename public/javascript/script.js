ed5814af3bc0b99447c9de0ec24cd26261316ab2
Pieter Verroken <pieterverroken@hotmail.com>
2014-12-26 19:09:33 +0100

    Added filtering on effects

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -9,8 +9,60 @@ $(document).ready(function(){
     
     
     $(".dropdown-menu").on('click', 'li a', function(){
-        $(this).parent().parent().prev().html($(this).html()+ ' <span class="caret"></span>');
+        $(this).parent().parent().prev().html('<span class="ef-chosen">'+$(this).html()+ '</span> <span class="caret"></span>');
+
+        var type = $('.dropdown-type > .ef-chosen').html();
+        var minRating = $('.dropdown-rating > .ef-chosen').find('.fa-star').length;
+
+        $('.ef-item-container > .ef-item').filter(function(){
+            removeWrongEffects($(this), type, minRating);
+        });
     });
+
+    function removeWrongEffects(elem, type, rating){
+        if(elem.find('.ef-rating').find('.fa-star').length >= rating){
+            if(elem.find('.ef-type').html() == type || type == "All types"){
+                elem.css("display","block");
+            }
+            else {
+                elem.css("display","none");
+            }
+        }
+        else {
+            elem.css("display","none");
+        }
+    }
+
+    /*$('.dropdown-menu.dropdown-type').on('click', 'li a', function(){
+        var effect = $(this).html();
+       // alert(effect);
+        $('.ef-item-container > .ef-item').filter(function(){
+           removeWrongType($(this),effect);
+        });
+    });
+
+    function removeWrongType(elem, eff){
+        if(elem.find('.ef-type').html() != eff){
+            elem.css("display","none");
+        }
+
+    }
+
+    $('.dropdown-menu.dropdown-rating').on('click', 'li a', function(){
+        var rating = $(this).find('.fa-star').length;
+        //alert(rating);
+
+        $('.ef-item-container > .ef-item').filter(function(){
+            //alert($(this).find('.ef-rating').find('.fa-star').length);
+            removeWrongRating($(this), rating);
+        });
+    });
+
+    function removeWrongRating(elem, rating){
+        if(elem.find('.ef-rating').find('.fa-star').length < rating){
+            elem.css("display","none");
+        }
+    }*/
 
 
     var socket = io.connect("http://127.0.0.1:8080/");
